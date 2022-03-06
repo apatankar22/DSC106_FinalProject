0f72da49dc29b76878c0a9f6543955649535cafb
Amogh Patankar <apatankar@ucsd.edu>
2022-03-06 13:46:06 -0800

    working on transition

--- app.js
+++ app.js
@@ -28,16 +28,17 @@ var scatter = function(filepath){
         svg1.append("text").attr("class", "x_label").attr("text-anchor", "end").attr("x", width - 100).attr("y", height + 40).text("Suicides per 100K Individuals");
         svg1.append("text").attr("class", "y_label").attr("text-anchor", "end").attr("x", width - 450).attr("y", -70).attr("dy", ".75em").attr("transform", "rotate(-90)").text("GDP per capita");
 
-        /*x.domain([0, 30])
-        svg.select(".x_label").transition().duration(2000)
+        x = d3.scaleLinear().domain([0, 30])
+        svg1.select("x_label").transition().duration(1000)
             .attr("opacity", "1")
+            .text("Suicides per 100K Individuals")
             .call(d3.axisBottom(x));
 
-        svg.selectAll("circle").transition().delay(function(d,i){return(i*3)}).duration(2000)
+        svg1.selectAll("circle").transition().delay(function(d,i){return(i*3)}).duration(1000)
             .attr("cx", function (d) { return x(d.suicides_100k);})
             .attr("cy", function (d) { return y(d.gdp_capita); })
 
-
+        
         // x axis (population) vs y axis (gdp_capita)
         var margin = {top: 10, right: 30, bottom: 75, left: 80}, width = 460 - margin.left - margin.right, height = 400 - margin.top - margin.bottom;
         var svg2 = d3.select("#scatterplot").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -49,27 +50,24 @@ var scatter = function(filepath){
         svg2.append("g").call(d3.axisLeft(y));
 
         svg2.append('g').selectAll("dot").data(data).enter().append("circle").attr("cx", function (d) {return x(d.population);}).attr("cy", function (d) {return y(d.gdp_capita);}).attr("r", 1.5).style("fill", "rgb(0,0,0)");
-        svg2.append("text").attr("class", "x label").attr("text-anchor", "end").attr("x", width - 100).attr("y", height + 60).text("Suicides per 100K Individuals");
-        svg2.append("text").attr("class", "y label").attr("text-anchor", "end").attr("x", width - 450).attr("y", -70).attr("dy", ".75em").attr("transform", "rotate(-90)").text("GDP per capita");
+        svg2.append("text").attr("class", "x_label").attr("text-anchor", "end").attr("x", width - 100).attr("y", height + 60).text("Population");
+        svg2.append("text").attr("class", "y_label").attr("text-anchor", "end").attr("x", width - 450).attr("y", -70).attr("dy", ".75em").attr("transform", "rotate(-90)").text("GDP per capita");
+        
 
-        x.domain([0, 4000])
-        svg.select(".myXaxis")
-            .transition()
-            .duration(2000)
+        x = d3.scaleLinear().domain([0, 43000000])
+        svg2.select("x_label").transition().duration(1000)
             .attr("opacity", "1")
+            .text("Population")
             .call(d3.axisBottom(x));
 
-        svg.selectAll("circle")
-            .transition()
-            .delay(function(d,i){return(i*3)})
-            .duration(2000)
-            .attr("cx", function (d) { return x(d.GrLivArea); } )
-            .attr("cy", function (d) { return y(d.SalePrice); } )*/
+        svg2.selectAll("circle").transition().delay(function(d,i){return(i*3)}).duration(1000)
+            .attr("cx", function (d) { return x(d.population);})
+            .attr("cy", function (d) { return y(d.gdp_capita); })
     })
 }
 
 var grouped_bar = function(filepath){
-    var data = d3.csv(filepath, function(i){
+    /*var data = d3.csv(filepath, function(i){
         return {"": parseInt(i[""]), country: i.country, year: parseInt(i.year), sex: i.sex, age: i.age, suicide_num: parseInt(i.suicides_no), population: i.population, suicides_100k: parseInt(i.suicides_100k), gdp_yearly: i.gdp_yearly, gdp_capita: i.gdp_capita, gen: i.generation}
     });
 
@@ -86,5 +84,5 @@ var grouped_bar = function(filepath){
         //rolled = d3.rollup(data, v => d3.sum(v, d => d.suicide_num), d => d.country);
         rolled = d3.rollup(data, v => v.length, d => d.year);
         console.log(rolled);
-    })    
+    })*/
 }
