4d9203affd21e8b37c28ab193738b9a66a904357
Amogh Patankar <apatankar@ucsd.edu>
2022-03-10 09:26:29 -0800

    fixed transition

--- app.js
+++ app.js
@@ -14,22 +14,21 @@ var scatter = function(filepath){
 
     data.then(function(data){
         // x axis (suicides/100k) vs y axis (gdp_capita)
-        var margin = {top: 10, right: 30, bottom: 60, left: 80}, width = 460 - margin.left - margin.right, height = 400 - margin.top - margin.bottom;
+        var margin = {top: 50, right: 30, bottom: 60, left: 80}, width = 460 - margin.left - margin.right, height = 400 - margin.top - margin.bottom;
         var svg1 = d3.select("#scatterplot").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
         
-        var x = d3.scaleLinear().domain([0, 30]).range([0, width]);
+        var x = d3.scaleLinear().domain([0, 0]).range([0, width]);
         var y = d3.scaleLinear().domain([0, 65000]).range([height, 0]);
         
-        svg1.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x));
         svg1.append("g").call(d3.axisLeft(y));
 
-
         svg1.append('g').selectAll("dot").data(data).enter().append("circle").attr("cx", function (d) {return x(d.suicides_100k);}).attr("cy", function (d) {return y(d.gdp_capita);}).attr("r", 2.0).style("fill", "rgb(104, 179, 163)");
-        svg1.append("text").attr("class", "x_label").attr("text-anchor", "end").attr("x", width - 100).attr("y", height + 40).text("Suicides per 100K Individuals");
-        svg1.append("text").attr("class", "y_label").attr("text-anchor", "end").attr("x", width - 450).attr("y", -70).attr("dy", ".75em").attr("transform", "rotate(-90)").text("GDP per capita");
+        svg1.append("text").attr("class", "xlabel").attr("text-anchor", "end").attr("x", width - 80).attr("y", height + 40).text("Suicides per 100K Individuals");
+        svg1.append("text").attr("class", "ylabel").attr("text-anchor", "end").attr("x", width - 450).attr("y", -70).attr("dy", ".75em").attr("transform", "rotate(-90)").text("GDP per capita");
 
-        x = d3.scaleLinear().domain([0, 30])
-        svg1.select("x_label").transition().duration(1000)
+        x.domain([0, 30])
+        svg1.append("g").attr("class", "xlabel").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x));
+        svg1.select("xlabel").transition().duration(1000)
             .attr("opacity", "1")
             .text("Suicides per 100K Individuals")
             .call(d3.axisBottom(x));
@@ -38,23 +37,26 @@ var scatter = function(filepath){
             .attr("cx", function (d) { return x(d.suicides_100k);})
             .attr("cy", function (d) { return y(d.gdp_capita); })
 
+
+
+
         
         // x axis (population) vs y axis (gdp_capita)
         var margin = {top: 10, right: 30, bottom: 75, left: 80}, width = 460 - margin.left - margin.right, height = 400 - margin.top - margin.bottom;
         var svg2 = d3.select("#scatterplot").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
         
-        var x = d3.scaleLinear().domain([0, 43000000]).range([0, width]);
+        var x = d3.scaleLinear().domain([0, 0]).range([0, width]);
         var y = d3.scaleLinear().domain([0, 65000]).range([height, 0]);
         
-        svg2.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x)).selectAll("text").style("text-anchor", "end").attr("dx", "-.8em").attr("dy", ".15em").attr("transform", "rotate(-30)");;
         svg2.append("g").call(d3.axisLeft(y));
 
         svg2.append('g').selectAll("dot").data(data).enter().append("circle").attr("cx", function (d) {return x(d.population);}).attr("cy", function (d) {return y(d.gdp_capita);}).attr("r", 2.0).style("fill", "rgb(104, 179, 163)");
-        svg2.append("text").attr("class", "x_label").attr("text-anchor", "end").attr("x", width - 100).attr("y", height + 60).text("Population");
+        svg2.append("text").attr("class", "x_label").attr("text-anchor", "end").attr("x", width - 150).attr("y", height + 60).text("Population");
         svg2.append("text").attr("class", "y_label").attr("text-anchor", "end").attr("x", width - 450).attr("y", -70).attr("dy", ".75em").attr("transform", "rotate(-90)").text("GDP per capita");
         
 
-        x = d3.scaleLinear().domain([0, 43000000])
+        x.domain([0, 45000000]);
+        svg2.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x)).selectAll("text").style("text-anchor", "end").attr("dx", "-.8em").attr("dy", ".15em").attr("transform", "rotate(-30)");
         svg2.select("x_label").transition().duration(1000)
             .attr("opacity", "1")
             .text("Population")
